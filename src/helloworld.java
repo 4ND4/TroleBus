48f0013565983656cc1bdf702f5d101167730717
yakubu <domayaks@yahoo.com>
2016-02-03 18:27:55 +0000

    new function

---
+++ src/helloworld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by yakubu on 03/02/2016.
+ */
+import java.util.Scanner;
+public class helloworld {
+     int func(int a, int b ){
+         a= a+b;
+         return a;
+     }
+
+}
+
